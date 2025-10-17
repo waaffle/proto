e581aeb410701fbe911108056c95d9fe9de936e2
Yana Suslyakova <susljakovajana@yandex.ru>
2025-10-17 16:27:43 +0300

    generated new files

--- gen/go/sso/auth.pb.go
+++ gen/go/sso/auth.pb.go
@@ -179,7 +179,7 @@ func (x *LoginRequest) GetAppId() int32 {
 
 type LoginResponse struct {
 	state         protoimpl.MessageState `protogen:"open.v1"`
-	Token         int32                  `protobuf:"varint,1,opt,name=token,proto3" json:"token,omitempty"`
+	Token         string                 `protobuf:"bytes,1,opt,name=token,proto3" json:"token,omitempty"`
 	unknownFields protoimpl.UnknownFields
 	sizeCache     protoimpl.SizeCache
 }
@@ -214,11 +214,11 @@ func (*LoginResponse) Descriptor() ([]byte, []int) {
 	return file_sso_auth_proto_rawDescGZIP(), []int{3}
 }
 
-func (x *LoginResponse) GetToken() int32 {
+func (x *LoginResponse) GetToken() string {
 	if x != nil {
 		return x.Token
 	}
-	return 0
+	return ""
 }
 
 type IsAdminRequest struct {
@@ -324,7 +324,7 @@ const file_sso_auth_proto_rawDesc = "" +
 	"\bpassword\x18\x02 \x01(\tR\bpassword\x12\x14\n" +
 	"\x05appId\x18\x03 \x01(\x05R\x05appId\"%\n" +
 	"\rLoginResponse\x12\x14\n" +
-	"\x05token\x18\x01 \x01(\x05R\x05token\"(\n" +
+	"\x05token\x18\x01 \x01(\tR\x05token\"(\n" +
 	"\x0eIsAdminRequest\x12\x16\n" +
 	"\x06userId\x18\x01 \x01(\x03R\x06userId\"+\n" +
 	"\x0fIsAdminResponse\x12\x18\n" +
